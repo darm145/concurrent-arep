59eb4adce1c8cc5c853322908796c4fcbe559f1f
darm145 <david.ramirez-mo@mail.escuelaing.edu.co>
2019-09-04 23:02:05 -0500

    port heroku

--- src/main/java/edu/escuelaing/arep/AppServer.java
+++ src/main/java/edu/escuelaing/arep/AppServer.java
@@ -16,7 +16,7 @@ public class AppServer {
 
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(4567);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port");
             System.exit(1);
@@ -111,5 +111,13 @@ public class AppServer {
         }
         
         
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+        // returns default port if heroku-port isn't set (i.e. on localhost)
+
     }
 }
