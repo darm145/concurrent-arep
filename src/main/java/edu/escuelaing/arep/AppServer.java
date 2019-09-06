ace77d7aad1c2d5b5b34a0579d852c4d87378055
david <david.ramirez-mo@mail.escuelaing.edu.co>
2019-09-06 14:04:09 -0500

    boton + index

--- src/main/java/edu/escuelaing/arep/AppServer.java
+++ src/main/java/edu/escuelaing/arep/AppServer.java
@@ -16,7 +16,6 @@ import java.lang.reflect.Method;
 public class AppServer {
     private static HashMap<String, Handler> listaURLHandler = new HashMap<String, Handler>();
 
-    
     public static void main(String[] args) throws Exception {
 
         ServerSocket serverSocket = null;
@@ -60,8 +59,10 @@ public class AppServer {
     }
 
     /**
-     *Metodo que permite guardar los metodos web de un POJO para que sean accesibles mas facilmente 
-     * @param route la direccion del POJO 
+     * Metodo que permite guardar los metodos web de un POJO para que sean
+     * accesibles mas facilmente
+     * 
+     * @param route la direccion del POJO
      */
     public static void inicializar(String route) throws Exception {
 
@@ -75,42 +76,59 @@ public class AppServer {
 
         }
         // Class[] argTypes = new Class[] { String[].class };
-       
 
-    } 
+    }
+
     /**
-     *Metodo que Desglosa una solicitud hecha por el browser y llama las funciones segun sea el caso
-     * @param request la solicitud hecha por el browser 
-     * @param out un writer que permite escribir archivos sencillos como texto al ususario
-     * @param clientOutput stream que permite escribir archivos complejos como imagenes al usuario
+     * Metodo que Desglosa una solicitud hecha por el browser y llama las funciones
+     * segun sea el caso
+     * 
+     * @param request      la solicitud hecha por el browser
+     * @param out          un writer que permite escribir archivos sencillos como
+     *                     texto al ususario
+     * @param clientOutput stream que permite escribir archivos complejos como
+     *                     imagenes al usuario
      */
     private static void handleRequest(String request, PrintWriter out, OutputStream clientOutput) throws IOException {
         try {
             String[] parts = request.trim().split("\n");
             String route = parts[0].split(" ")[1];
             String[] elements = route.split("/");
-            String element = elements[elements.length - 1];
-
-            if (element.endsWith(".jpg")) {
-                handleImage(element, clientOutput, out);
-            } else if (element.endsWith(".html")) {
-                handleHtml(element, out, clientOutput);
-            } else if (route.contains("/app")) {
-                handleMethod(elements, out, clientOutput);
+            
+            if (elements.length == 0) {
+                handleHtml("index.html", out, clientOutput);
             } else {
-                Error404(clientOutput);
+                String element = elements[elements.length - 1];
+
+                if (element.endsWith(".jpg")) {
+                    handleImage(element, clientOutput, out);
+                } else if (element.endsWith(".html")) {
+                    handleHtml(element, out, clientOutput);
+                } else if (route.contains("/app")) {
+                    handleMethod(elements, out, clientOutput);
+                } else {
+                    Error404(clientOutput);
+                }
+
             }
+
             out.close();
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("error en consulta");
         }
 
     }
+
     /**
-     *funcion para manejar metodos recibidos en la URL bajo la secuencia/app/{clase}/{metodo}
-     * @param elements lista que contiene los elementos de la URL separados por "/"
-     * @param out un writer que permite escribir archivos sencillos como texto al ususario
-     * @param clientOutput stream que permite escribir archivos complejos como imagenes al usuario
+     * funcion para manejar metodos recibidos en la URL bajo la
+     * secuencia/app/{clase}/{metodo}
+     * 
+     * @param elements     lista que contiene los elementos de la URL separados por
+     *                     "/"
+     * @param out          un writer que permite escribir archivos sencillos como
+     *                     texto al ususario
+     * @param clientOutput stream que permite escribir archivos complejos como
+     *                     imagenes al usuario
      */
     private static void handleMethod(String[] elements, PrintWriter out, OutputStream clientOutput) throws IOException {
         if (elements.length == 4) {
@@ -118,12 +136,12 @@ public class AppServer {
             String clase = elements[2];
             String metodo = elements[3];
             String key = "/app/" + clase + "/" + metodo;
-            
+
             if (!listaURLHandler.containsKey(key)) {
                 try {
-                   
+
                     inicializar("edu.escuelaing.arep." + clase);
-                    
+
                 } catch (Exception e) {
                     Error404(clientOutput);
                     return;
@@ -139,7 +157,7 @@ public class AppServer {
                     out.print("\r\n");
                     out.print(result);
                 } else {
-                    key=key.split("\\?")[0];
+                    key = key.split("\\?")[0];
                     String paramString = elements[3].split("\\?")[1];
                     String[] paramValues = paramString.split("&");
                     Object[] params = new Object[paramValues.length];
@@ -147,7 +165,7 @@ public class AppServer {
                         params[i] = paramValues[i].split("=")[1];
                     }
                     System.out.println(key);
-                    String result=listaURLHandler.get(key).procesarConParams(params);
+                    String result = listaURLHandler.get(key).procesarConParams(params);
                     out.print("HTTP/1.1 200 OK \r\n");
                     out.print("Content-Type: text/html \r\n");
                     out.print("\r\n");
@@ -163,11 +181,15 @@ public class AppServer {
             Error404(clientOutput);
         }
     }
+
     /**
-     *funcion para manejar los archivos HTML recibidos en la URL
-     * @param element nombre del archivo html a buscar
-     * @param out un writer que permite escribir archivos sencillos como texto al ususario
-     * @param clientOutput stream que permite escribir archivos complejos como imagenes al usuario
+     * funcion para manejar los archivos HTML recibidos en la URL
+     * 
+     * @param element      nombre del archivo html a buscar
+     * @param out          un writer que permite escribir archivos sencillos como
+     *                     texto al ususario
+     * @param clientOutput stream que permite escribir archivos complejos como
+     *                     imagenes al usuario
      */
 
     private static void handleHtml(String element, PrintWriter out, OutputStream clientOutput) throws IOException {
@@ -187,11 +209,15 @@ public class AppServer {
         }
 
     }
+
     /**
-     *funcion para manejar imagenes recibidos en la url
-     * @param element nombre de la imagen a buscar
-     * @param out un writer que permite escribir archivos sencillos como texto al ususario
-     * @param clientOutput stream que permite escribir archivos complejos como imagenes al usuario
+     * funcion para manejar imagenes recibidos en la url
+     * 
+     * @param element      nombre de la imagen a buscar
+     * @param out          un writer que permite escribir archivos sencillos como
+     *                     texto al ususario
+     * @param clientOutput stream que permite escribir archivos complejos como
+     *                     imagenes al usuario
      */
 
     private static void handleImage(String element, OutputStream clientOutput, PrintWriter out) throws IOException {
@@ -212,9 +238,13 @@ public class AppServer {
         }
 
     }
+
     /**
-     *funcion para mostrar un Error 404 en caso de que algun fichero o metodo no sea encontrado
-     * @param clientOutput stream que permite escribir archivos complejos como imagenes al usuario
+     * funcion para mostrar un Error 404 en caso de que algun fichero o metodo no
+     * sea encontrado
+     * 
+     * @param clientOutput stream que permite escribir archivos complejos como
+     *                     imagenes al usuario
      */
 
     private static void Error404(OutputStream clientOutput) throws IOException {
@@ -233,8 +263,9 @@ public class AppServer {
                 + "</html>");
 
     }
+
     /**
-     *funcion para obtener un puerto por el cual el servidor va a trabajar
+     * funcion para obtener un puerto por el cual el servidor va a trabajar
      */
     static int getPort() {
         if (System.getenv("PORT") != null) {
